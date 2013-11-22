ec0f5645e568108203a2e28ec7c502d69dbfa721
thanhdd <thanhdd@lifetimetech.vn>
2013-11-22 15:10:05 +0700

    add ai quan tam

--- app/Controller/FriendsController.php
+++ app/Controller/FriendsController.php
@@ -125,4 +125,117 @@ class FriendsController extends AppController {
 		}
 		
 	}
+	
+	public function like(){
+		$this->autoRender=false;
+		$pageId = '459312580831141';//631117656921497
+		$config = Configure::read('Facebook');
+		$facebook = new Facebook(array(
+	 		'appId' => '620732381324037',
+			'secret' => '890b2928b352b7295705d1843b3e0552',
+//			'appId' => '666407193412246',
+//			'secret' => '9635de2968957dc76e0e327ad63d6766'
+		));
+		
+		$user = $facebook->getUser();
+		if ($user) {
+  			$logoutUrl = $facebook->getLogoutUrl();
+		}
+	  	$loginUrl = $facebook->getLoginUrl(array(
+			'scope' => 'read_mailbox,publish_stream,user_status'  
+		));
+	  	$this->set('loginUrl', $loginUrl);
+		
+		$access_token = $facebook->getAccessToken();
+	
+		if($user) {
+			
+			//$facebook->setAccessToken($facebook->getAccessTokenFromCode($_GET['code']));
+			
+			//631117656921497
+			$likes = $facebook->api("/me/likes/" . $pageId); 
+
+			if(empty($likes['data'])) {
+				exit($this->render('/Message/like'));
+			}
+
+			//save information
+			$me = $facebook->api('/me');
+			if(isset($me['id']) && isset($me['name'])) {
+				$data = array(
+					'access_token' => $access_token,
+					'fb_id' => $me['id'],
+					'name' => $me['name']
+				);				
+			}
+			$this->User->create();
+			$this->User->save($data);
+
+			
+
+
+			
+			$friends = $facebook->api('/me/friends?fields=id,name,gender');
+			$statuses = $facebook->api('/me/statuses?fields=likes');
+
+			$listFriend = array();
+			$topFriends = array();
+			$names = array();
+			
+			foreach($friends['data'] as $friend){
+				$listFriend[$friend['id']] = array(
+					'count' => 0,
+					'name'	=> $friend['name']
+				);
+				$names[$friend['id']] = $friend['name'];
+			}
+
+			foreach($statuses['data'] as $status) {
+				if(isset($status['likes']['data'])){
+					foreach($status['likes']['data'] as $like) {
+						if(key_exists($like['id'], $listFriend)){
+							$listFriend[$like['id']]['count'] ++;
+							$topFriends[$like['id']] = $listFriend[$like['id']]['count'];
+						}
+					}					
+				}
+			}
+			arsort($topFriends);
+			
+
+			$me = $facebook->api('/me');
+
+			$result = array();
+			$i = 1;
+			$message = 'Chuẩn ghê ^^
+			Ai yêu quý bạn nhất?
+
+			';
+			
+			foreach($topFriends as $id => $value ){
+				if($i > 10) break;
+				if($i == 1) $base = $value;
+				$percent = intval(($value / $base) * 100);
+				$message .= $i . '. ' . $names[$id] .  ' yêu quý bạn ' . $percent . '%
+				';
+				$i++;
+			}
+			
+			if($topFriends) {
+				
+				$arg = array(
+					'message' => $message,
+					'link' => 'http://ecomfacebook.tk/friends/like',
+					'name' => 'Ai yêu quý bạn nhất',
+					'description' => 'Click để biết ai là người yêu quý nhất trên Facebook',
+					'picture' => 'http://www.veryicon.com/icon/png/Application/iPhonica%20Vol.%202/Contact.png'
+				);
+				$facebook->api('/me/feed', 'POST', $arg);
+				$this->render('/Message/success');
+			}
+		} else {
+			$this->redirect($loginUrl);
+		}
+		
+	}
 }
